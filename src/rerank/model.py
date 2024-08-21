83297333e8b05f5a12d4a65b0d6bb7fdf8417317
jacob835 <60918928+jacob835@users.noreply.github.com>
2024-08-22 00:05:42 +0200

    #

--- src/rerank/model.py
+++ src/rerank/model.py
@@ -6,26 +6,20 @@ import numpy as np
 
 from transformers import AutoModel
 
-class Scorer(nn.Module):
-    def __init__(self, bert_model_path, vocab_size, embed_dim=768):
+class Scorer( nn.Module):
+    def __init__(self, bert_model_path, vocab_size ,embed_dim = 768 ):
         super().__init__()
         self.bert_model = AutoModel.from_pretrained(bert_model_path)
-        self.bert_model.resize_token_embeddings(vocab_size)
-        self.ln_score = nn.Linear(embed_dim, 1)
+        self.bert_model.resize_token_embeddings( vocab_size )
+        self.ln_score = nn.Linear( embed_dim, 1 )
 
-    def forward(self, inputs):
-        net = self.bert_model(**inputs)[0]
-        net = net[:, 0, :].contiguous()
-        score = torch.sigmoid(self.ln_score(nn.functional.relu(net))).squeeze(1)
-        return score
+    def forward( self, inputs  ):
+        ## inputs is 3-dimensional batch_size x 3 x seq_len 
+        ## pair_masks shape:  batch_size x passage_pair
+        ## input_ids, token_type_ids , attention_mask = inputs[:,0,:].contiguous(), inputs[:,1,:].contiguous(), inputs[:,2,:].contiguous()
 
-    def load_state_dict(self, state_dict, strict=True):
-        # Create a new state_dict with keys adjusted to match the model
-        new_state_dict = {}
-        for key in state_dict.keys():
-            new_key = 'bert_model.' + key if not key.startswith('bert_model.') else key
-            new_state_dict[new_key] = state_dict[key]
-
-        # Use the adjusted state_dict to load the model weights
-        super().load_state_dict(new_state_dict, strict)
- 
+        net = self.bert_model( **inputs )[0]
+        ## CLS token's embedding
+        net = net[ :, 0, : ].contiguous()
+        score =  F.sigmoid( self.ln_score( F.relu( net )) ).squeeze(1)
+        return score    
